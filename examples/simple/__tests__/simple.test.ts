8e83a3766ec28724923cba6eecf8e49b97656baa
Michael Oppitz <michael@shapediver.com>
2021-02-22 12:18:25 +0100

    headless chrome tests

--- examples/simple/__tests__/simple.test.ts
+++ examples/simple/__tests__/simple.test.ts
@@ -1,8 +1,21 @@
-describe('test', () => {
-    // TODO
-    beforeEach(() => {
+import puppeteer from 'puppeteer';
+
+let page: puppeteer.Page;
+let browser: puppeteer.Browser;
+
+describe('ShapeDiver', () => {
+    beforeAll(async () => {
+        browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+        page = await browser.newPage();
+        await page.goto('http://127.0.0.1:8080/dist-prod/index.html');
     });
 
-    it('test', async () => {
+    it('should be titled "ShapeDiver"', async () => {
+        await new Promise<void>((resolve) => setTimeout(() => {resolve()}, 2000));
+
+        await page.screenshot({path: 'test.png'});
+
+        await expect(page.title()).resolves.toMatch('ShapeDiver');
+        await browser.close();
     });
-})
+});
