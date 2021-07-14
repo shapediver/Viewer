67e4b4cdc6b600fc19163a1f5dc432b95d7f1b26
Michael Oppitz <michael@shapediver.com>
2021-07-14 19:19:39 +0200

    throwing input validation error

--- shared/utils/src/input-validator/InputValidator.ts
+++ shared/utils/src/input-validator/InputValidator.ts
@@ -17,7 +17,7 @@ export class InputValidator {
     public validateAndError(topic: LOGGINGTOPIC, scope: string, value: any, type: Types, defined: boolean = true, enumValues: string[] = []) {
         const res = this.validate(value, type, defined, enumValues);
         if(res) return;
-        this._logger.error(topic, new SDError(`${scope}: Input could not be validated. ${value} is not of type ${type}.${defined === false ? ' (Can also be undefined)' : ''}`));
+        this._logger.error(topic, new SDError(`${scope}: Input could not be validated. ${value} is not of type ${type}.${defined === false ? ' (Can also be undefined)' : ''}`), `${scope}: Input could not be validated. ${value} is not of type ${type}.${defined === false ? ' (Can also be undefined)' : ''}`, true);
     }
 
     private validate(value: any, stringLiteral: Types, defined: boolean = true, enumValues: string[] = []): boolean {
