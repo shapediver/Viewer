ffb4b5a71f5f93e6fddfdbf4aa29e5f017e8b985
Michael Oppitz <michael@shapediver.com>
2021-08-05 07:28:05 +0200

    event-types udpated

--- shared/services/src/event-engine/EventTypes.ts
+++ shared/services/src/event-engine/EventTypes.ts
@@ -6,27 +6,11 @@ export enum CAMERA {
 export enum ENVIRONMENTMAP {
     ENVIRONMENTMAP_LOADED = "environmentmap.loaded",
 }
-export enum HTML {
-    // HTML_ANCHORADD = "html.anchoradd",
-    // HTML_ANCHORREMOVE = "html.anchorremove",
-}
-export enum INTERACTION {}
-export enum LIGHT {
-    LIGHT_ADDED = "light.added",
-    LIGHT_REMOVED = "light.removed",
-    LIGHT_CHANGED = "light.changed",
-    LIGHT_SCENE_ADDED = "light.scene.added",
-    LIGHT_SCENE_REMOVED = "light.scene.removed",
-    LIGHT_SCENE_CHANGED = "light.scene.changed",
-}
 export enum RENDERING {
     BEAUTY_RENDERING_FINISHED = "rendering.beautyrenderingfinished"
-    //RENDERING_FRAMERATE = "rendering.framerate",
 }
 export enum SCENE {
     SCENE_BOUNDING_BOX_CHANGE = "scene.boundingboxchange"
-    // SCENE_VISIBLE = "scene.visible",
-    // SCENE_HIDDEN = "scene.hidden",
 }
 export enum VIEWER {
     VIEWER_CREATED = "viewer.created",
@@ -37,19 +21,6 @@ export enum VIEWER {
 
 
 
-export enum EXPORT {
-    // EXPORT_AVAILABLE = "export.available",
-    // EXPORT_REGISTERED = "export.registered",
-    // EXPORT_REGISTEREDBATCH = "export.registeredbatch",
-    // EXPORT_UPDATE = "export.update",
-    // EXPORT_STATUS = "export.status",
-}
-export enum PARAMETER {
-    // PARAMETER_REGISTERED = "parameter.registered",
-    // PARAMETER_REGISTEREDBATCH = "parameter.registeredbatch",
-    // PARAMETER_UPDATE = "parameter.update",
-    // PARAMETER_VALUEUPDATE = "parameter.valueupdate",
-}
 export enum SESSION {
     SESSION_CREATED = "session.created",
     SESSION_INITIALIZED = "session.initialized",
@@ -57,9 +28,6 @@ export enum SESSION {
     SESSION_CUSTOMIZED = "session.customized",
     SESSION_CLOSED = "session.closed"
 }
-
-
-
 export enum SETTINGS {
     SETTINGS_REGISTERED = "settings.registered",
     SETTINGS_UPDATE = "settings.update",
@@ -70,11 +38,6 @@ export enum SETTINGS {
 export const EVENTTYPE = { 
     CAMERA, 
     ENVIRONMENTMAP,
-    EXPORT, 
-    HTML, 
-    INTERACTION, 
-    LIGHT, 
-    PARAMETER, 
     RENDERING, 
     SCENE, 
     SESSION,
@@ -83,4 +46,4 @@ export const EVENTTYPE = {
 };
 export type EVENTTYPE = typeof EVENTTYPE;
 
-export type MAINEVENTTYPE = typeof CAMERA | typeof ENVIRONMENTMAP | typeof EXPORT | typeof HTML | typeof INTERACTION | typeof LIGHT | typeof PARAMETER | typeof RENDERING | typeof SCENE | typeof SESSION | typeof SETTINGS | typeof VIEWER;
+export type MAINEVENTTYPE = typeof CAMERA | typeof ENVIRONMENTMAP | typeof RENDERING | typeof SCENE | typeof SESSION | typeof SETTINGS | typeof VIEWER;
