26e7f7ee14d632428cf390d2411759a6a206bee6
Michael Oppitz <michael@shapediver.com>
2021-10-04 10:21:51 +0200

    deploy example script

--- scripts/deploy-example.ts
+++ scripts/deploy-example.ts
@@ -4,6 +4,7 @@ import pako from 'pako';
 
 const recursiveReadSync = require('recursive-readdir-sync');
 const { exec } = require("child_process");
+const readline = require('readline');
 
 const execPromise = (cmd: string) => {
     return new Promise((resolve, reject) => {
@@ -16,19 +17,46 @@ const execPromise = (cmd: string) => {
 
 (async () => {
     try {
+
+        const rl1 = readline.createInterface({ input: process.stdin, output: process.stdout });
+        let name;
+        await new Promise<void>((resolve) => {
+            rl1.question('Enter the name of the page to be deployed\n', (answer: string) => {
+                name = answer;
+                rl1.close();
+                resolve();
+            });
+        });
+
+        const rl2 = readline.createInterface({ input: process.stdin, output: process.stdout });
+        let example: string = '';
+        await new Promise<void>((resolve) => {
+            rl2.question('Enter the name of the example to be deployed\n', (answer: string) => {
+                if (['ar', 'ar-query', 'attributes', 'compare', 'gltf', 'multiple', 'performance', 'query', 'simple', 'static', 'test'].includes(answer)) {
+                    example = answer;
+                } else {
+                    throw new Error('Invalid version, has to be major, minor or patch.')
+                }                
+                rl2.close();
+                resolve();
+            });
+        });
+
+        console.log(name, example)
+
         // console.log(await execPromise('npm run build-current'));
-        console.log(await execPromise('cd examples/simple && npm run build-prod && cd ../..'));
+        console.log(await execPromise('cd examples/' + example + ' && npm run build-prod && cd ../..'));
 
         const bucketName = 'shapediverviewer';
-        const prefixLatest = 'v3/signify/';
+        const prefixLatest = 'v3/examples/' + name + '/';
         const s3 = new AWS.S3({ maxRetries: 5 });
 
-        const directoryPathTest = 'examples/simple/dist-prod/';
+        const directoryPathTest = 'examples/' + example + '/dist-prod/';
         const fileContentsTest = <string[]>recursiveReadSync(directoryPathTest);
         fileContentsTest.map(function (f, cb) {
             s3.putObject({
                 Bucket: bucketName,
-                Key: prefixLatest + 'simple/' + f.substring(directoryPathTest.length, f.length).replace(/\\/g, '/'),
+                Key: prefixLatest + example + '/' + f.substring(directoryPathTest.length, f.length).replace(/\\/g, '/'),
                 Body: pako.gzip(fs.readFileSync(f)),
                 ACL: 'public-read',
                 ContentType: f.endsWith('.js') || f.endsWith('.js.map') ? 'text/javascript' : f.endsWith('.html') ? 'text/html' : 'text/plain',
