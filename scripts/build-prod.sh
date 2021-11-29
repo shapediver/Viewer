0f81b233fa8a3073cffa72b1824b00ad9f84985e
Michael Oppitz <michael@shapediver.com>
2021-11-29 12:37:06 +0100

    interactions cdn changes

--- scripts/build-prod.sh
+++ scripts/build-prod.sh
@@ -1,4 +1,13 @@
+WEBPACK_PATH=$1
+
 rm -rf ./dist-prod
 mkdir dist-prod
 cp index.html dist-prod/index.html
-webpack --config ../../webpack.prod.js --output-filename bundle.js --output-path dist-prod
+
+echo ${WEBPACK_PATH}
+if [ -n "$WEBPACK_PATH" ]
+then
+    webpack --config $WEBPACK_PATH --output-filename bundle.js --output-path dist-prod
+else
+    webpack --config ../../webpack.prod.js --output-filename bundle.js --output-path dist-prod
+fi
