ea5c1f351191773c3a3ef8f38b08b4cf03edd207
Michael Oppitz <michael@shapediver.com>
2021-12-16 11:30:23 +0100

    typedoc config

--- typedoc.js
+++ typedoc.js
@@ -1,11 +1,16 @@
 module.exports = {
     entryPoints: ['./api/api/src/index.ts', './shared/node-tree/src/index.ts'],
     out: 'docs',
-    exclude: ['**/node_modules/**'],
+    exclude: [
+        '**/node_modules/**/*',
+        '**/__tests__/**/*',
+        '**/tests/**/*'
+    ],
     name: 'Viewer',
-    excludePrivate: true,
     hideGenerator: true,
     disableSources: true,
     theme: 'minimal',
+    excludeExternals: true,
+    excludePrivate: true,
 }
 //    "doc": "s --theme minimal --name Viewer && cp -r api/api/images docs/images",
