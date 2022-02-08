162d8a06fda443a0ae6484b4cc3612e7fce4cc8d
Michael Oppitz <michael@shapediver.com>
2022-02-08 11:07:19 +0100

    bugfix: transformations

--- data-engine/data-engine/src/DataEngine.ts
+++ data-engine/data-engine/src/DataEngine.ts
@@ -64,13 +64,13 @@ export class DataEngine {
                     const t = content.transformations[i];
                     if (Array.isArray(t) && t.length === 16) {
                         const nodeInstance = node.cloneInstance();
-                        nodeInstance.transformations.push({
+                        nodeInstance.transformations = [{
                             id: 'content_' + i,
                             matrix: mat4.fromValues(t[0], t[1], t[2], t[3],
                                 t[4], t[5], t[6], t[7],
                                 t[8], t[9], t[10], t[11],
                                 t[12], t[13], t[14], t[15])
-                        })
+                        }].concat(node.transformations);
                         transformationNode.addChild(nodeInstance)
                     }
                 }
