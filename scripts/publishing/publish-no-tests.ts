fd10bb2dea49a66b8731109f54bb3c0306565b2a
Michael Oppitz <michael@shapediver.com>
2022-07-04 20:46:20 +0200

    Revert "publishing fix"
    This reverts commit 9b06d5975a01240f1d50134a652b57e65f2224b5.

--- scripts/publishing/publish-no-tests.ts
+++ scripts/publishing/publish-no-tests.ts
@@ -3,79 +3,78 @@ import { execPromise, deployToS3, getDirectories, readAnswerOptions, readAnswer 
 
 (async () => {
     try {
-        // /**
-        //  * How do we increment the version?
-        //  */
-        // let version = await readAnswerOptions('Which part of the version would you like to increment? (major, minor, patch)\n', ['major', 'minor', 'patch'])
+        /**
+         * How do we increment the version?
+         */
+        let version = await readAnswerOptions('Which part of the version would you like to increment? (major, minor, patch)\n', ['major', 'minor', 'patch'])
         
-        // let github_publish_answer = await readAnswer('Publish to github?\n');
-        // let github_publish = (github_publish_answer === 'yes' || github_publish_answer === 'y');
+        let github_publish_answer = await readAnswer('Publish to github?\n');
+        let github_publish = (github_publish_answer === 'yes' || github_publish_answer === 'y');
 
-        // let npm_publish_answer = await readAnswer('Publish to npm?\n');
-        // let npm_publish = (npm_publish_answer === 'yes' || npm_publish_answer === 'y');
+        let npm_publish_answer = await readAnswer('Publish to npm?\n');
+        let npm_publish = (npm_publish_answer === 'yes' || npm_publish_answer === 'y');
 
 
-        // const changes = await execPromise(`git status --porcelain`);
-        // if(changes) {
-        //     throw new Error(`Please stage and commit your files first.\n${changes}`);
-        // } else {
-        //     console.log(changes);
-        // }
+        const changes = await execPromise(`git status --porcelain`);
+        if(changes) {
+            throw new Error(`Please stage and commit your files first.\n${changes}`);
+        } else {
+            console.log(changes);
+        }
 
-        // console.log('checking versioning...')
+        console.log('checking versioning...')
 
-        // /**
-        //  * Increase the version
-        //  */
-        // const packageJson = require('../../main/default/package.json');
-        // const versions: string[] = packageJson.version.split('.');
-        // const newVersion: string =  (+versions[0] + ((version === 'major' || version === 'premajor') ? 1 : 0)) + '.' + 
-        //                     ((version === 'major' || version === 'premajor') ? 0 : (+versions[1] + ((version === 'minor' || version === 'preminor') ? 1 : 0))) + '.' + 
-        //                     ((version === 'major' || version === 'premajor') ? 0 : (version === 'minor' || version === 'preminor') ? 0 : (+versions[2] + ((version === 'patch' || version === 'prerelease') ? 1 : 0)));
+        /**
+         * Increase the version
+         */
+        const packageJson = require('../../main/default/package.json');
+        const versions: string[] = packageJson.version.split('.');
+        const newVersion: string =  (+versions[0] + ((version === 'major' || version === 'premajor') ? 1 : 0)) + '.' + 
+                            ((version === 'major' || version === 'premajor') ? 0 : (+versions[1] + ((version === 'minor' || version === 'preminor') ? 1 : 0))) + '.' + 
+                            ((version === 'major' || version === 'premajor') ? 0 : (version === 'minor' || version === 'preminor') ? 0 : (+versions[2] + ((version === 'patch' || version === 'prerelease') ? 1 : 0)));
 
-        // const git_commit: string = <string>await execPromise('git rev-parse HEAD');
-        // const git_branch: string = <string>await execPromise('git branch --show-current');
-        // if (!git_branch || !git_commit) throw new Error('Could not get git branch or commit for deployment.');
-        // const timestamp = new Date().toISOString();
+        const git_commit: string = <string>await execPromise('git rev-parse HEAD');
+        const git_branch: string = <string>await execPromise('git branch --show-current');
+        if (!git_branch || !git_commit) throw new Error('Could not get git branch or commit for deployment.');
+        const timestamp = new Date().toISOString();
 
-        // fs.writeFileSync('shared/build-data/src/build_data.ts', 'export const build_data = ' + JSON.stringify({
-        //     build_version: '3.' + newVersion,
-        //     build_date: timestamp,
-        //     build_branch: git_branch,
-        //     build_commit: git_commit
-        // }, null, 0) + ';');
+        fs.writeFileSync('shared/build-data/src/build_data.ts', 'export const build_data = ' + JSON.stringify({
+            build_version: '3.' + newVersion,
+            build_date: timestamp,
+            build_branch: git_branch,
+            build_commit: git_commit
+        }, null, 0) + ';');
         
-        // console.log('re-building for deployment...')
-        // console.log(await execPromise('npm run build-current'));
-        // console.log(await execPromise('npm run build-prod'));
+        console.log('re-building for deployment...')
+        console.log(await execPromise('npm run build-current'));
+        console.log(await execPromise('npm run build-prod'));
 
-        // console.log('creating doc...')
-        // console.log(await execPromise('npm run doc'));
+        console.log('creating doc...')
+        console.log(await execPromise('npm run doc'));
 
-        // console.log('creating automatic pre-publishing commit...')
-        // console.log(await execPromise('git add .'));
-        // console.log(await execPromise('git commit -m "automatic pre-publishing commit"'));
+        console.log('creating automatic pre-publishing commit...')
+        console.log(await execPromise('git add .'));
+        console.log(await execPromise('git commit -m "automatic pre-publishing commit"'));
 
-        // console.log(await execPromise(`lerna version ${version} --yes --no-private --exact --force-publish`));
-        // console.log(await execPromise('git tag -l "@shapediver*" | xargs -n 1 git push --delete origin'));
-        // console.log(await execPromise('git tag -l "@shapediver*" | xargs git tag -d'));
+        console.log(await execPromise(`lerna version ${version} --yes --no-private --exact --force-publish`));
+        console.log(await execPromise('git tag -l "@shapediver*" | xargs -n 1 git push --delete origin'));
+        console.log(await execPromise('git tag -l "@shapediver*" | xargs git tag -d'));
 
-        // console.log(await execPromise(`npm whoami`));
+        console.log(await execPromise(`npm whoami`));
 
-        if(true) {
+        if(npm_publish) {
             console.log('publishing to npm...')
             console.log(await execPromise(`lerna publish from-package --yes --no-private --force-publish --registry https://registry.npmjs.org/`));
         }
 
-        if(true) {
+        if(github_publish) {
             console.log('publishing to github...')
             console.log(await execPromise(`lerna publish from-package --yes --no-private --force-publish --registry https://npm.pkg.github.com/`));
         }
         
-        const newVersion = "2.0.1"
         const prefix = 'v3/' + newVersion;
       
-        if(true) {
+        if(npm_publish) {
             console.log('deploying to s3...')
             deployToS3('docs', 'api', prefix, true)
 
