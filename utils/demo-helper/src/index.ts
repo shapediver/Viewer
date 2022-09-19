0c400430a449764b0815c294b9b6a6a9f5c279da
Michael Oppitz <michael@shapediver.com>
2022-09-19 13:48:44 +0200

    new demo utils package

---
+++ utils/demo-helper/src/index.ts
@@ -0,0 +1,16 @@
+import { StageManager, Stage, IStageData } from "./core/StageManager";
+import { createCustomUi, IBooleanElement, IDropdownElement, ISliderElement, IStringElement, updateCustomUi } from "./ui/CustomUI";
+import { createUi, updateUi } from "./ui/ShapeDiverUI";
+
+export {
+    StageManager, Stage, IStageData
+}
+
+export {
+    createUi, updateUi
+}
+
+export {
+    createCustomUi, updateCustomUi,
+    ISliderElement, IDropdownElement, IStringElement, IBooleanElement
+}
