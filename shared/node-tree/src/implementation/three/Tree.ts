4b8e14b0fd44f0717a8be2cd29b2d664a05d5829
Michael Oppitz <michael@shapediver.com>
2022-10-06 12:37:00 +0200

    scene tree singleton fix

--- shared/node-tree/src/implementation/three/Tree.ts
+++ shared/node-tree/src/implementation/three/Tree.ts
@@ -1,8 +1,10 @@
+import { singleton } from "tsyringe";
 import { ITreeNodeThreeJs } from "../../interfaces/three/ITreeNodeThreeJs";
 import { ITreeThreeJs } from "../../interfaces/three/ITreeThreeJs";
 import { AbstractTree } from "../AbstractTree";
 import { TreeNodeThreeJs } from "./TreeNodeThreejs";
 
+@singleton()
 export class Tree extends AbstractTree<ITreeNodeThreeJs> implements ITreeThreeJs {
     constructor() {
         super(new TreeNodeThreeJs('root'))
