22ce67cdf46ae9c17aa8c6711448beab7ae7a2c2
Michael Oppitz <michael@shapediver.com>
2023-01-16 16:59:27 +0100

    bugfix: three.js version update

--- shared/types/src/implementation/material/MaterialStandardData.ts
+++ shared/types/src/implementation/material/MaterialStandardData.ts
@@ -7,7 +7,7 @@ export class MaterialStandardData extends AbstractMaterialData implements IMater
   // #region Properties (26)
 
   #attenuationColor: string = '#ffffff';
-  #attenuationDistance = 0.0;
+  #attenuationDistance = Infinity;
   #clearcoat: number = 0;
   #clearcoatMap?: IMapData;
   #clearcoatNormalMap?: IMapData;
@@ -353,7 +353,7 @@ export class MaterialStandardData extends AbstractMaterialData implements IMater
     this.side = MATERIAL_SIDE.DOUBLE;
     
     this.attenuationColor = '#ffffff';
-    this.attenuationDistance = 1;
+    this.attenuationDistance = Infinity;
     this.clearcoat = 0;
     this.clearcoatMap = undefined;
     this.clearcoatNormalMap = undefined;
