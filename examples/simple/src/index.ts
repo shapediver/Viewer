35e033c5cbaa37ee1798e2e1e92203b39ce75275
Michael Oppitz <michael@shapediver.com>
2023-01-19 08:47:56 +0100

    locherschmuck texture dragging

--- examples/simple/src/index.ts
+++ examples/simple/src/index.ts
@@ -1,16 +1,174 @@
+import {
+    createViewport,
+    createSession,
+    IOutputApi,
+    ITreeNode,
+    addListener,
+    EVENTTYPE_INTERACTION,
+    IEvent,
+    ORTHOGRAPHIC_CAMERA_DIRECTION,
+    VISIBILITY_MODE
+} from "@shapediver/viewer";
+import {
+    InteractionEngine,
+    DragManager,
+    InteractionData,
+    PlaneConstraint
+} from "@shapediver/viewer.features.interaction";
+import { createMenu, outputNames, positionAdjustementCallback, ringBoundaryBB, setOutputRestrictions, textureBoundaryBB, updateBB } from "./utilities";
 
-import * as SDV from '@shapediver/viewer';
+import * as SDV from "@shapediver/viewer"
 
 (<any>window).SDV = SDV;
 
 (async () => {
-    let viewport = await SDV.createViewport({
-        id: 'myViewport',
-        canvas: <HTMLCanvasElement>document.getElementById('canvas')
-    })
-    let session = await SDV.createSession({
-        id: 'mySession',
-        ticket: '7d6061acf274727aff4710230595ff9e58fbd019a1e173ccd5f2342ecc697fd2397ab08cadc3014b2760f858d18b4aade0aade39fd73a5c1b44fef4d5a457739c1fe28ec6b44ef593a41f6c0cccc78fb3f62234080db167d60c23886b32c759068cdff6af5a8e3-853d465964df80e5db72abe9655cedee',
-        modelViewUrl: 'https://sdeuc1.eu-central-1.shapediver.com'
-    })
+    /**
+     * INITIAL VIEWPORT AND SESSION SETUP
+     * 
+     * First we create two viewports, one for displaying the texture and the boundary, and one for displaying the ring.
+     * 
+     * Then we create the session with either a ticket that was provided via a query parameter (?ticket=THE_TICKET) or a ticket that is hard coded.
+     */
+
+    // create the texture viewport, this one will be used to show the texture and boundary
+    const textureViewport = await createViewport({
+        canvas: document.getElementById("canvas1") as HTMLCanvasElement,
+        id: "textureViewport",
+        visibility: VISIBILITY_MODE.MANUAL
+    });
+
+    // create the ring viewport, this one will be used to show the ring
+    const ringViewport = await createViewport({
+        canvas: document.getElementById("canvas2") as HTMLCanvasElement,
+        id: "ringViewport",
+        visibility: VISIBILITY_MODE.MANUAL
+    });
+
+    // read out the query parameter "ticket" if it exists, use it as a ticket
+    const params = new Proxy(new URLSearchParams(window.location.search), {
+        get: (searchParams, prop) => searchParams.get(<string>prop),
+    });
+    let ticket = decodeURIComponent((<any>params).ticket);
+    ticket = ticket === "null" ? "59fd4e1c733f89001a2b432cdaf6544a5fa5092ab6d8e1273deb811ce59e03414edf41de717c038657e9d0ec0cc8d93c270e7f4abe9607ff3cec64edfaab511276d5cd44feb5c4247e14f1d3d29991138a2f10f8e276253842bb8fac50b62c2f30971c3acbd04c-c6f1337fa6d0c30bb3dd769aa4a0b095" : ticket;
+
+    // create the session
+    const session = await createSession({
+        ticket,
+        modelViewUrl: "https://sdr7euc1.eu-central-1.shapediver.com",
+        id: "mySession"
+    });
+
+    /**
+     * OUTPUT ADJUSTEMENT AND MENU CREATION
+     * 
+     * In this step we store values for convience in different data structures:
+     *  - We read the parameters into variables.
+     *  - We store all needed outputs into a dictionary.
+     *  - We set the viewport restrictions for the outputs.
+     *  - We create a menu to be able to adjust the texture_rotation and TS (texture scale).
+     */
+
+    // read out the two parameters that are going to be used in this example
+    const textureRotationParameter = session.getParameterByName('texture_rotation')[0];
+    const textureMoveParameter = session.getParameterByName('texture_move')[0];
+    const textureScaleParameter = session.getParameterByName('TS')[0];
+
+    // storage of the outputs as defined by Edwin
+    // see the list of outputs in the utilities file the variable "outputNames"
+    let outputs: {
+        [key: string]: IOutputApi;
+    } = {};
+
+    // store all non-material outputs (the ones that don't have the format "material" in their format array)
+    outputNames.forEach(
+        (n) =>
+        (outputs[n] = session
+            .getOutputByName(n)
+            .find((o) => !o.format.includes("material"))!)
+    );
+
+    // set the output restrictions for each viewport so that the texture and boundary are only visible in one
+    // and the ring with the profile in the other
+    setOutputRestrictions(outputs, textureViewport, ringViewport);
+
+    // create a menu that makes the adjustment of the texture rotation and texture scale possible
+    createMenu(session, textureRotationParameter, textureScaleParameter, textureMoveParameter)
+
+    /**
+     * INTERACTION SETUP
+     * 
+     * In the textureViewport we create an orthographic camera to make the interaction easier.
+     * 
+     * We create the InteractionEngine, a DragManager and a Plane contraint to only be able to drag the texture on that plane.
+     * We then add the InteractionData to the texture output to make it draggable.
+     */
+
+    // change the camera of the texture viewport to an orthographic one
+    const camera = textureViewport.createOrthographicCamera();
+    camera.direction = ORTHOGRAPHIC_CAMERA_DIRECTION.FRONT;
+    textureViewport.assignCamera(camera.id);
+
+    // create the interactionEngine and provide it the texture viewport object
+    const interactionEngine = new InteractionEngine(textureViewport);
+
+    // create the dragManager and add it
+    const dragManager = new DragManager();
+    interactionEngine.addInteractionManager(dragManager);
+
+    // add a plane constraint
+    const planeConstraint = new PlaneConstraint([0, 1, 0], [0, 0, 0]);
+    // use the token to remove the constraint again (removeDragConstraint)
+    const token = dragManager.addDragConstraint(planeConstraint);
+
+    // here we add the InteractionData to the 2d_texture output node
+    // we use the updateCallback for this to always add it again once it has been updated
+    // as there was previously already an updateCallback set in setOutputRestrictions, we call it again here to keep that behavior as well
+    const interactionData = new InteractionData({ drag: true });
+    const originalUpdateCallback = outputs["2d_texture"].updateCallback;
+    outputs["2d_texture"].updateCallback = (newNode?: ITreeNode, oldNode?: ITreeNode) => {
+        if(originalUpdateCallback)
+            originalUpdateCallback(newNode, oldNode)
+        if (newNode) 
+            newNode.data.push(interactionData);
+    };
+    // we call it once in the beginning to initially assign the InteractionData
+    outputs["2d_texture"].updateCallback!(outputs["2d_texture"].node);
+
+    /**
+     * INTERACTION EVENT LISTENERS
+     * 
+     * In this step we create the event listeners for the interactions.
+     */
+    
+    // the DRAG_START listener
+    const dragStartListenerToken = addListener(EVENTTYPE_INTERACTION.DRAG_START, (e: IEvent) => {
+        // whenever the dragging start, adjust the bounding boxes with the current values
+        // these bounding boxes are used to check if the texture is within the specified area
+        updateBB(outputs);
+    });
+
+    // the DRAG_MOVE listener
+    const dragMoveListenerToken = addListener(EVENTTYPE_INTERACTION.DRAG_MOVE, (e: IEvent) => {
+        // whenever the texture was move, see if it is within the specified area, and if not, restrict it
+        positionAdjustementCallback(e);
+    });
+
+    // the DRAG_END listener
+    const dragEndListenerToken = addListener(EVENTTYPE_INTERACTION.DRAG_END, async (e: IEvent) => {
+        // when the dragging ends, restrict the texture position for a last time
+        const matrix = positionAdjustementCallback(e);
+        // create a BB that is transformed by the current dragging
+        const draggedTextureBoundaryBB = textureBoundaryBB
+            .clone()
+            .applyMatrix(matrix);
+        // read out the center of the dragged texture BB
+        const center = draggedTextureBoundaryBB.boundingSphere.center;
+        // apply the position to the move parameters and customize the scene
+        textureMoveParameter.value = `[${center[0]}, ${center[2]}]`;
+        await session.customize();
+    });
+
+    // once everything has been initialize, show the viewports
+    textureViewport.show = true;
+    ringViewport.show = true;
 })();
---
+++ examples/simple/src/utilities.ts
@@ -0,0 +1,206 @@
+import { mat4, vec3 } from "gl-matrix";
+import { Box, IEvent, IOutputApi, IParameterApi, ISessionApi, ITreeNode, IViewportApi, PARAMETER_TYPE } from "@shapediver/viewer";
+import { IDragEvent } from "@shapediver/viewer.features.interaction";
+
+/** 
+ * The outputs that were provided me by Edwin that should exist in this model.
+ */
+export const outputNames = [
+    "2d_ring_boundary",
+    "2d_texture_boundary",
+    "2d_ring",
+    "2d_texture",
+    "G_A_Flächen",
+    "G_A_Linien",
+    "G_A_Vermassung",
+    "G_B_2"
+];
+
+/**
+ * The outputs that are used when displaying the ring.
+ */
+export const ringDisplayOutputNames = [
+    "G_A_Flächen",
+    "G_A_Linien",
+    "G_A_Vermassung",
+    "G_B_2"
+];
+
+// the BB of the current texture boundary
+export let textureBoundaryBB: Box = new Box();
+// the BB of the current ring boundary
+export let ringBoundaryBB: Box = new Box();
+
+/**
+ * Create a a menu for the "texture_rotation" and "TS" (scale) parameters.
+ * When the values are updated, also reset the "texture_move" parameter and then customize the scene.
+ * The "texture_move" parameter has to be reset, because after rotating the texture vertically, 
+ * the position can be out of the ring boundary with the geometry coming from the server.
+ * The same issue exists for the scale.
+ * 
+ * @param session 
+ * @param textureRotationParameter 
+ * @param textureMoveParameter 
+ */
+export const createMenu = (session: ISessionApi, textureRotationParameter: IParameterApi<any>, textureScaleParameter: IParameterApi<number>, textureMoveParameter: IParameterApi<string>) => {
+    const menuDiv = <HTMLDivElement>document.getElementById('menu');
+
+    const textureRotationLabel = <HTMLLabelElement>document.createElement('label');
+    textureRotationLabel.innerText = "Rotation";
+    menuDiv.appendChild(textureRotationLabel);
+    
+    const textureRotationSelect = document.createElement('select');
+    menuDiv.appendChild(textureRotationSelect);
+
+    for(let i = 0; i < textureRotationParameter.choices!.length; i++) {
+        const rotationOption = document.createElement('option');
+        textureRotationSelect.appendChild(rotationOption);
+
+        rotationOption.setAttribute("value", i + "");
+        rotationOption.setAttribute("name", textureRotationParameter.choices![i]);
+        rotationOption.innerHTML = textureRotationParameter.choices![i];
+        if (textureRotationParameter.value == i) rotationOption.setAttribute("selected", "");
+    }
+
+    textureRotationSelect.onchange = async () => {
+        textureRotationParameter.value = textureRotationSelect.value;
+        textureMoveParameter.value = "[0,0]";
+        await session.customize();
+    }
+
+    const textureScaleLabel = <HTMLLabelElement>document.createElement('label');
+    textureScaleLabel.innerText = "Scale";
+    menuDiv.appendChild(textureScaleLabel);
+
+    const textureScaleInput = <HTMLInputElement>document.createElement("input");
+    textureScaleInput.setAttribute("type", "range");
+    textureScaleInput.setAttribute("min", textureScaleParameter.min !== undefined ? textureScaleParameter.min + "" : textureScaleParameter.min + "");
+    textureScaleInput.setAttribute("max", textureScaleParameter.max !== undefined ? textureScaleParameter.max + "" : textureScaleParameter.max + "");
+    textureScaleInput.setAttribute("value", textureScaleParameter.value + "");
+    if (textureScaleParameter.type === PARAMETER_TYPE.INT) {
+        textureScaleInput.setAttribute("step", "1");
+    } else if (textureScaleParameter.type === PARAMETER_TYPE.EVEN || textureScaleParameter.type === PARAMETER_TYPE.ODD) {
+        textureScaleInput.setAttribute("step", "2");
+    } else {
+        textureScaleInput.setAttribute("step", 1 / Math.pow(10, textureScaleParameter.decimalplaces!) + "");
+    }
+    menuDiv.appendChild(textureScaleInput);
+
+    textureScaleInput.onchange = async () => {
+        textureScaleParameter.value = textureScaleInput.value;
+        textureMoveParameter.value = "[0,0]";        
+        await session.customize();
+    };
+
+}
+
+/**
+ * Set the restriction for all ringDisplayOutputNames only to be shown in the ringViewport
+ * and all others to be shown only in the textureViewport.
+ * 
+ * @param outputs 
+ * @param textureViewport 
+ * @param ringViewport 
+ */
+export const setOutputRestrictions = (
+    outputs: { [key: string]: IOutputApi },
+    textureViewport: IViewportApi,
+    ringViewport: IViewportApi
+) => {
+    ringDisplayOutputNames.forEach((n) => {
+        outputs[n].updateCallback = (newNode?: ITreeNode, oldNode?: ITreeNode) => {
+            if (newNode) {
+                newNode.excludeViewports.push(textureViewport.id);
+                newNode.updateVersion();
+            }
+        };
+        outputs[n].updateCallback!(outputs[n].node);
+    });
+
+    outputNames.forEach((n) => {
+        if (ringDisplayOutputNames.includes(n)) return;
+        outputs[n].updateCallback = (newNode?: ITreeNode, oldNode?: ITreeNode) => {
+            if (newNode) {
+                newNode.excludeViewports.push(ringViewport.id);
+                newNode.updateVersion();
+            }
+        };
+        outputs[n].updateCallback!(outputs[n].node);
+    });
+};
+
+/**
+ * Update the BB of the texture boundary and the ring boundary 
+ * according the the data of the outputs "2d_texture_boundary" and "2d_ring_boundary", respectively.
+ * 
+ * @param outputs 
+ */
+export const updateBB = (outputs: { [key: string]: IOutputApi }) => {
+    const textureBoundary: number[][] = outputs["2d_texture_boundary"].content![0]
+        .data;
+    const textureBoundaryBBMin = vec3.fromValues(Infinity, 0, Infinity);
+    const textureBoundaryBBMax = vec3.fromValues(-Infinity, 0, -Infinity);
+    textureBoundary.forEach((p) => {
+        if (p[0] < textureBoundaryBBMin[0]) textureBoundaryBBMin[0] = p[0];
+        if (p[2] < textureBoundaryBBMin[2]) textureBoundaryBBMin[2] = p[2];
+        if (p[0] > textureBoundaryBBMax[0]) textureBoundaryBBMax[0] = p[0];
+        if (p[2] > textureBoundaryBBMax[2]) textureBoundaryBBMax[2] = p[2];
+    });
+    textureBoundaryBB = new Box(textureBoundaryBBMin, textureBoundaryBBMax);
+
+    const ringBoundary: number[][] = outputs["2d_ring_boundary"].content![0].data;
+    const ringBoundaryBBMin = vec3.fromValues(Infinity, 0, Infinity);
+    const ringBoundaryBBMax = vec3.fromValues(-Infinity, 0, -Infinity);
+    ringBoundary.forEach((p) => {
+        if (p[0] < ringBoundaryBBMin[0]) ringBoundaryBBMin[0] = p[0];
+        if (p[2] < ringBoundaryBBMin[2]) ringBoundaryBBMin[2] = p[2];
+        if (p[0] > ringBoundaryBBMax[0]) ringBoundaryBBMax[0] = p[0];
+        if (p[2] > ringBoundaryBBMax[2]) ringBoundaryBBMax[2] = p[2];
+    });
+    ringBoundaryBB = new Box(ringBoundaryBBMin, ringBoundaryBBMax);
+};
+
+/**
+ * A callback that is execture on DRAG_MOVE and DRAG_END events.
+ * 
+ * The current drag matrix is used to create an intermediate bounding box 
+ * that is used to evaluate it the texture is still within the ring boundary.
+ * 
+ * If this is not the case, the matrix is adjusted and the texture node is being updated.
+ * 
+ * @param e 
+ * @returns 
+ */
+export const positionAdjustementCallback = (e: IEvent): mat4 => {
+    const dragEvent = <IDragEvent>e;
+
+    const dragTransformation = dragEvent.node.getTransformation('SD_drag_matrix')!;
+    dragTransformation.matrix[13] = 0;
+
+    const draggedTextureBoundaryBB = textureBoundaryBB
+        .clone()
+        .applyMatrix(dragTransformation.matrix);
+
+    let changed = false;
+    if (draggedTextureBoundaryBB.min[0] > ringBoundaryBB.min[0]) {
+        changed = true;
+        dragTransformation.matrix[12] = ringBoundaryBB.min[0]- textureBoundaryBB.min[0];
+    }
+    if (draggedTextureBoundaryBB.max[0] < ringBoundaryBB.max[0]) {
+        changed = true;
+        dragTransformation.matrix[12] = ringBoundaryBB.max[0] - textureBoundaryBB.max[0];
+    }
+    if (draggedTextureBoundaryBB.min[2] > ringBoundaryBB.min[2]) {
+        changed = true;
+        dragTransformation.matrix[14] = ringBoundaryBB.min[2] - textureBoundaryBB.min[2];
+    }
+    if (draggedTextureBoundaryBB.max[2] < ringBoundaryBB.max[2]) {
+        changed = true;
+        dragTransformation.matrix[14] = ringBoundaryBB.max[2] - textureBoundaryBB.max[2];
+    }
+
+    if (changed)
+        dragEvent.node.updateVersion();
+
+    return dragTransformation.matrix;
+}
