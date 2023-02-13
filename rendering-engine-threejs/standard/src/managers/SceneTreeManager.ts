9b476ac5437126ac0adee6e0c22d98b08efcff1b
Michael Oppitz <michael@shapediver.com>
2023-02-13 10:43:59 +0100

    bugfix: clear color

--- rendering-engine-threejs/standard/src/managers/SceneTreeManager.ts
+++ rendering-engine-threejs/standard/src/managers/SceneTreeManager.ts
@@ -66,7 +66,7 @@ export class SceneTreeManager implements IManager {
     // #region Constructors (1)
 
     constructor(private readonly _renderingEngine: RenderingEngine) {
-        this._scene.background = this._renderingEngine.createThreeJsColor('#ffffff');
+        this._scene.background = new THREE.Color('#ffffff');
     }
 
     // #endregion Constructors (1)
