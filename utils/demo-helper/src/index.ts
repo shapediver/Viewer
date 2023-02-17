375430c5789a10ff8c9eeaa43efa8080e8bd454c
Michael Oppitz <michael@shapediver.com>
2023-02-17 09:02:58 +0100

    demo helpers color swatch

--- utils/demo-helper/src/index.ts
+++ utils/demo-helper/src/index.ts
@@ -1,5 +1,5 @@
 import { StageManager, Stage, IStageData } from "./core/StageManager";
-import { createCustomUi, IBooleanElement, IDropdownElement, ISliderElement, IStringElement, updateCustomUi } from "./ui/CustomUI";
+import { createCustomUi, IBooleanElement, IDropdownElement, ISliderElement, IStringElement, IColorElement, updateCustomUi } from "./ui/CustomUI";
 import { createUi, updateUi } from "./ui/ShapeDiverUI";
 
 export {
@@ -12,5 +12,5 @@ export {
 
 export {
     createCustomUi, updateCustomUi,
-    ISliderElement, IDropdownElement, IStringElement, IBooleanElement
+    ISliderElement, IDropdownElement, IStringElement, IBooleanElement, IColorElement
 }
