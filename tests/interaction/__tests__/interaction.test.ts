9d3d7298baeab11c33add5afff437f85459b39ab
Michael Oppitz <michael@shapediver.com>
2023-05-24 14:34:08 +0200

    test adaptations

--- tests/interaction/__tests__/interaction.test.ts
+++ tests/interaction/__tests__/interaction.test.ts
@@ -35,21 +35,21 @@ describe('device testing', () => {
 
         // await actions.move({ origin: bottomImage }).press().pause(1000).release().pause(1000).perform()
         // await actions.clear()
-        await actions.move({ origin: bottomImage }).press().pause(1000).move({ x: 640 * 1.5, y: 400 * 1.5 }).release().pause(1000).perform()
+        await actions.move({ origin: bottomImage }).press().pause(1000).move({ x: Math.round(640 * 0.75), y: Math.round(400 * 0.75) }).release().pause(1000).perform()
         await actions.clear()
-        await actions.move({ origin: bottomImage }).press().pause(1000).move({ x: 840 * 1.5, y: 450 * 1.5 }).release().pause(1000).perform()
+        await actions.move({ origin: bottomImage }).press().pause(1000).move({ x: Math.round(840 * 0.75), y: Math.round(450 * 0.75) }).release().pause(1000).perform()
         await actions.clear()
-        await actions.move({ origin: bottomImage }).press().pause(1000).move({ x: 540 * 1.5, y: 400 * 1.5 }).release().pause(1000).perform()
+        await actions.move({ origin: bottomImage }).press().pause(1000).move({ x: Math.round(540 * 0.75), y: Math.round(400 * 0.75) }).release().pause(1000).perform()
         await actions.clear()
     
         let topImage = driver.findElement(By.id('top'));
         actions = driver.actions({ async: true, bridge: true });
 
-        await actions.move({ origin: topImage }).press().pause(1000).move({ x: 640 * 1.5, y: 300 * 1.5 }).release().pause(1000).perform()
+        await actions.move({ origin: topImage }).press().pause(1000).move({ x: Math.round(640 * 0.75), y: Math.round(300 * 0.75) }).release().pause(1000).perform()
         await actions.clear()
-        await actions.move({ origin: topImage }).press().pause(1000).move({ x: 850 * 1.5, y: 380 * 1.5 }).release().pause(1000).perform()
+        await actions.move({ origin: topImage }).press().pause(1000).move({ x: Math.round(850 * 0.75), y: Math.round(380 * 0.75) }).release().pause(1000).perform()
         await actions.clear()
-        await actions.move({ origin: topImage }).press().pause(1000).move({ x: 540 * 1.5, y: 300 * 1.5 }).release().pause(1000).perform()
+        await actions.move({ origin: topImage }).press().pause(1000).move({ x: Math.round(540 * 0.75), y: Math.round(300 * 0.75) }).release().pause(1000).perform()
         await actions.clear()
     
 
