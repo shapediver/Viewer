8329105d1e486afa2d2ef47a77b34a4d17888a8a
Michael Oppitz <michael@shapediver.com>
2023-09-27 11:57:40 +0200

    debug console output

--- rendering-engine-threejs/standard/src/managers/postprocessing/ao/poissionDenoise/PoissionDenoisePass.ts
+++ rendering-engine-threejs/standard/src/managers/postprocessing/ao/poissionDenoise/PoissionDenoisePass.ts
@@ -135,6 +135,7 @@ export class PoissionDenoisePass extends Pass {
 	// #region Public Methods (4)
 
 	public generateDenoiseSamples(numSamples: number, numRings: number, r: number, texelSize: Vector2) {
+
 		const angleStep = (2 * Math.PI * numRings) / numSamples;
 		const invNumSamples = 1.0 / numSamples;
 		const radiusStep = invNumSamples;
@@ -152,6 +153,7 @@ export class PoissionDenoisePass extends Pass {
 			radius += radiusStep;
 			angle += angleStep;
 		}
+		console.log("DEBUG OUTPUT: Poission Disk", samples, numSamples, numRings, r, texelSize);
 
 		return samples;
 	}
