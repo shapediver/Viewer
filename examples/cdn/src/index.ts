c7fb66652430d35336236cd9f8ef88833a175433
Michael Oppitz <michael@shapediver.com>
2024-06-18 13:18:11 +0200

    drawing tools CDN

--- examples/cdn/src/index.ts
+++ examples/cdn/src/index.ts
@@ -2,7 +2,9 @@
 import * as SDV from '@shapediver/viewer';
 import * as SDVInteractions from '@shapediver/viewer.features.interaction';
 import * as SDVAttributeVisualization from '@shapediver/viewer.features.attribute-visualization';
+import * as SDVDrawingTools from '@shapediver/viewer.features.drawing-tools';
 
 (<any>window).SDV = SDV;
 (<any>window).SDVInteractions = SDVInteractions;
 (<any>window).SDVAttributeVisualization = SDVAttributeVisualization;
+(<any>window).SDVDrawingTools = SDVDrawingTools;
