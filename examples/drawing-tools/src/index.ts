8eb110620032ff8f631492ab1f4779ad2a9080d2
Michael Oppitz <michael@shapediver.com>
2024-06-19 13:28:03 +0200

    updated example

--- examples/drawing-tools/src/index.ts
+++ examples/drawing-tools/src/index.ts
@@ -1,10 +1,10 @@
 import * as SDV from '@shapediver/viewer';
+import { IDirectionalLightApi, LIGHT_TYPE } from '@shapediver/viewer';
 import {
     createDrawingTools,
     GeometryRestrictionApi,
     IDrawingToolsEvent,
     IDrawingToolsApi,
-    PlaneRestrictionApi,
     PointsData,
     RESTRICTION_TYPE,
     Settings
@@ -21,34 +21,82 @@ import { createCustomUi, IBooleanElement, ISliderElement } from '@shapediver/vie
     // create a session
     const session = await SDV.createSession({
         ticket:
-            '1636173cca09293804fb0084bb4726040b742d1a17fb18dd1801ef70a2cd3c76a45c2a442c6dc602495c43c39f2a5dd1e168a8f8f5edfebca7ad13b6b3fecdb60b0e1d914a8a25a1b16ba61ac9ff2a5a84e27cbfed31c5a4d1e5b416e78abd86a8f76a99b1eb5c-39e4dbd01454d7afbd92052722e1743f',
+            '34197323be1534c886330808d21d8b5f8cc79da35591fb865949f96163df04813c11cbdfc57543c229a62f9c30f9f80a071fdc1568eb40fae92b452ffb3cac54b9add5373f0def321838ada06dc6462c90da511b8161dda2c6249c90b2ef3aa12a7d2bce187ffb-84bf522404c7b78b236eae5b9efe9e27',
         modelViewUrl: 'https://sdr7euc1.eu-central-1.shapediver.com',
         id: 'mySession',
         initialParameterValues: {
-            'points': JSON.stringify({ 'points': [] })
+            'points': '{"points":[]}'
         }
     });
+    
+    const lightScene = viewport.lightScene!;
+    // we search for the lights that are directional and have shadows
+    const lightsWithShadows: IDirectionalLightApi[] = <IDirectionalLightApi[]>(
+      Object.values(lightScene.lights).filter(
+        (l) =>
+          l.type === LIGHT_TYPE.DIRECTIONAL &&
+          (<IDirectionalLightApi>l).castShadow === true
+      )
+    );
+  
+    for (let i = 0; i < lightsWithShadows.length; i++) {
+      // changing the shadow map resolution increases the qualtiy, but lowers the performance
+      // has to be power of two (default: 1024)
+      lightsWithShadows[i].shadowMapResolution = 1024;
+      // the shadow map bias is responsible for the offset of the shadow map
+      // if this would be 0, the object would cast a shadow on itself
+      lightsWithShadows[i].shadowMapBias = -0.0005;
+    }
 
-    const pointsParameter = session.getParameterByName('points')[0];
-
-    // get the output for the drawing tools options
-    const customizationProperties: Settings = (session.getOutputByName('DrawingToolsSettings')[0].content![0].data as any);
-    console.log('Customization properties', customizationProperties);
+    const customizationProperties: Settings = {
+        general: {
+            // If the drawing tool is updated automatically when the drawing is changed.
+            autoUpdate: true,
+            // The unit that will be displayed in the distance and point labels. 
+            displayUnit: 'm'
+        },
+        geometry: {
+            // If the line is automatically closed.
+            autoClose: false,
+            // The minimum amount of points
+            minPoints: 4,
+            // The maximum number of points
+            maxPoints: 25,
+        },
+        restrictions: {
+            // Add a geometry restriction
+            'geometry': {
+                type: RESTRICTION_TYPE.GEOMETRY,
+                // The node to restrict is set below
+                nodes: [],
+                wireframeColor: '#ffffff',
+            },
+            // Add an axis restriction
+            'axis': {
+                type: RESTRICTION_TYPE.AXIS
+            }
+        }
+    };
 
     /**
      * Callback function for the drawing tool
-     * executed when the drawing tool is finished
+     * executed when the drawing tool is updated
      * 
      * @param geometryData 
      */
     const onUpdate = async (pointsData: PointsData) => {
         console.log('Drawing tools updated', pointsData);
 
+        const pointsParameter = session.getParameterByName('points')[0];
         pointsParameter.value = JSON.stringify({ 'points': pointsData });
         await session.customize();
 
     };
 
+    /**
+     * Callback function for the drawing tool
+     * executed when the drawing tool is cancelled
+     */
     const onCancel = () => {
         console.log('Drawing tools cancelled');
 
@@ -79,23 +127,33 @@ import { createCustomUi, IBooleanElement, ISliderElement } from '@shapediver/vie
         sendNotification('Maximum points reached', (event as IDrawingToolsEvent).message!);
     });
 
-    const drawingToolsApi: IDrawingToolsApi | undefined = createDrawingTools(viewport, { onUpdate, onCancel }, {
-        geometry: {
-            minPoints: 4,
-            maxPoints: 10,
-            autoClose: false,
-            close: true,
-
-        }
+    SDV.addListener(SDV.EVENTTYPE_DRAWING_TOOLS.UNCLOSED_LOOP, (event: SDV.IEvent) => {
+        sendNotification('Line loop not closed', (event as IDrawingToolsEvent).message!);
     });
+
+
+    const drawingToolsApi: IDrawingToolsApi | undefined = createDrawingTools(viewport, { onUpdate, onCancel }, customizationProperties);
     (window as any).drawingToolsApi = drawingToolsApi;
+
+    const geometryRestrictionApi = drawingToolsApi.restrictions['geometry'] as GeometryRestrictionApi;
+
+    /**
+     * Set the node to restrict
+     */
+    const terrainOutput = session.getOutputByName('Terrain')[0];
+    const cb = (newNode?: SDV.ITreeNode) => {
+
+        if (!newNode)  return;
+        new Promise((resolve) => setTimeout(resolve, 0)).then(() => geometryRestrictionApi.updateNodes([newNode]));
+    };
+    terrainOutput.updateCallback = cb;
+    cb(terrainOutput.node);
 
     /**
      * 
      * RESTRICTION UI
      * 
      */
-    
     const menuDiv = document.createElement('div');
     menuDiv.id = 'menu';
     menuDiv.style.position = 'absolute';
@@ -123,6 +181,7 @@ import { createCustomUi, IBooleanElement, ISliderElement } from '@shapediver/vie
             drawingToolsApi!.showDistanceLabels = value;
         }
     });
+
     createCustomUi(elements, menuDiv);
 
     /**
@@ -146,5 +205,4 @@ import { createCustomUi, IBooleanElement, ISliderElement } from '@shapediver/vie
         }
     };
     document.body.appendChild(imgCameraSwitch);
-
 })();
