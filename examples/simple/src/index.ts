f62593f44e258bd54ea463ae7f2901de52bd7b3e
Michael Oppitz <michael@shapediver.com>
2024-08-19 11:01:45 +0200

    SS-7921: implemented new filename logic

--- examples/simple/src/index.ts
+++ examples/simple/src/index.ts
@@ -1,40 +1,19 @@
 import * as SDV from '@shapediver/viewer';
-import { addListener } from '@shapediver/viewer';
-import { Gumball } from '@shapediver/viewer.features.gumball';
-import { InteractionData, InteractionEngine, MultiSelectManager, InteractionEventResponseMapping, HoverManager, IMultiSelectEvent } from '@shapediver/viewer.features.interaction';
-import { createCustomUi, IBooleanElement } from '@shapediver/viewer.shared.demo-helper';
+import { createSession, createViewport } from '@shapediver/viewer';
+import { createUi } from '@shapediver/viewer.shared.demo-helper';
 
 (<any>window).SDV = SDV;
 
 (async () => {
-    const viewport = await SDV.createViewport({
+    const viewport = await createViewport({
         id: 'myViewport',
         canvas: <HTMLCanvasElement>document.getElementById('canvas')
     });
-    const session = await SDV.createSession({
+    const session = await createSession({
         id: 'mySession',
-        ticket: '3bd25bd6c28eb03a40868ff726a574fc7b53e02734af17066ac917c59cc1e55fcbab57acd8b9ff1e3ecfc3f64ae43f4787648b2c6951fc2c63df9164d4161954f2838906f0abd854d4cb778ce70b3914f6f9bb8e6da0969a23f90363f43aa2329fd93d9cf850db-0c977a4c28f85600ea079045f25c1182',
+        ticket: '50eb2a26ddaa432ca18288b8a120ef194fa35bb813e4f43ae89d657991a865f9deaa20a1c840e47cdf6dbc019cd16ae15a9a6b3a7d91722455299d6bd29b1f26b3ff3b7adaac1df3d50f3ba4d010a560180dff8f745c946dadb41167a3431e223d69b32743f167-5b9465f92a0cf9c235b8ea315aab0cd5',
         modelViewUrl: 'https://sdr7euc1.eu-central-1.shapediver.com'
     });
-
-    const doorsOutput = session.getOutputByName('Doors')[0];
-    doorsOutput.node!.getNodesByNameWithRegex(new RegExp(/^Door_/)).forEach(door => {
-        door.addData(new InteractionData({ hover: true, select: true }));
-        door.updateVersion();
-    });
-
-    const interactionEngine = new InteractionEngine(viewport);
-
-    const multiSelectManager = new MultiSelectManager();
-    multiSelectManager.useModifierKeys = true;
-    multiSelectManager.effectMaterial = new SDV.MaterialStandardData({ color: 'red' });
-    interactionEngine.addInteractionManager(multiSelectManager);
-
-    const hoverManager = new HoverManager();
-    hoverManager.effectMaterial = new SDV.MaterialStandardData({ color: 'blue' });
-    interactionEngine.addInteractionManager(hoverManager);
-
-
 
     const menuDiv = document.createElement('div');
     menuDiv.id = 'menu';
@@ -44,56 +23,5 @@ import { createCustomUi, IBooleanElement } from '@shapediver/viewer.shared.demo-
     menuDiv.style.zIndex = '100';
     document.body.appendChild(menuDiv);
 
-    let gumball: Gumball | undefined;
-
-    const eventListenerCallback = (e: IMultiSelectEvent) => {
-        console.log('MultiSelectEvent', e.nodes);
-        if (gumball) {
-            gumball.close();
-
-            while (menuDiv.firstChild)
-                menuDiv.removeChild(menuDiv.firstChild);
-        }
-
-        if (e.nodes.length > 0) {
-            console.log('Create Gumball');
-
-            gumball = new Gumball(viewport, e.nodes, { reuseTransformation: true });
-            createCustomUi([
-                <IBooleanElement>{
-                    name: 'enableTranslation',
-                    type: 'boolean',
-                    value: gumball.enableTranslation,
-                    onInputCallback: (value: boolean) => {
-                        gumball!.enableTranslation = value;
-                    }
-                },
-                <IBooleanElement>{
-                    name: 'enableRotation',
-                    type: 'boolean',
-                    value: gumball.enableRotation,
-                    onInputCallback: (value: boolean) => {
-                        gumball!.enableRotation = value;
-                    }
-                },
-                <IBooleanElement>{
-                    name: 'enableScaling',
-                    type: 'boolean',
-                    value: gumball.enableScaling,
-                    onInputCallback: (value: boolean) => {
-                        gumball!.enableScaling = value;
-                    }
-                },
-            ], menuDiv);
-        }
-    };
-
-    addListener(SDV.EVENTTYPE_INTERACTION.MULTI_SELECT_ON, (e) => {
-        const multiSelectEvent = e as InteractionEventResponseMapping[SDV.EVENTTYPE_INTERACTION.MULTI_SELECT_ON];
-        eventListenerCallback(multiSelectEvent);
-    });
-    addListener(SDV.EVENTTYPE_INTERACTION.MULTI_SELECT_OFF, (e) => {
-        const multiSelectEvent = e as InteractionEventResponseMapping[SDV.EVENTTYPE_INTERACTION.MULTI_SELECT_OFF];
-        eventListenerCallback(multiSelectEvent);
-    });
+    createUi(session, menuDiv);
 })();
