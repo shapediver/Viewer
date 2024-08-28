66013acc096fb5a120810a5d2b66d5dbb6565d66
Michael Oppitz <michael@shapediver.com>
2024-08-28 14:39:51 +0200

    gumball helper function

--- features/gumball/src/index.ts
+++ features/gumball/src/index.ts
@@ -2,6 +2,8 @@ import { Gumball } from './implementation/Gumball';
 import { GumballEventResponseMapping } from './interfaces/events/EventResponseMapping';
 import { IGumball } from './interfaces/IGumball';
 import { IGumballEvent } from './interfaces/events/IGumballEvent';
+import { updateGumballTransformation } from './implementation/updateGumballTransformation';
 
 export { IGumball, Gumball };
 export { GumballEventResponseMapping, IGumballEvent };
+export { updateGumballTransformation };
