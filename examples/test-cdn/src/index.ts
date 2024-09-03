6d14b2d44df5b76a95c178801c1ac4761ec16362
Michael Oppitz <michael@shapediver.com>
2024-09-03 11:45:49 +0200

    gumball CDN

--- examples/test-cdn/src/index.ts
+++ examples/test-cdn/src/index.ts
@@ -3,8 +3,11 @@ import * as SDV from '@shapediver/viewer';
 import * as SDVInteractions from '@shapediver/viewer.features.interaction';
 import * as SDVAttributeVisualization from '@shapediver/viewer.features.attribute-visualization';
 import * as SDVDrawingTools from '@shapediver/viewer.features.drawing-tools';
+import * as SDVGumball from '@shapediver/viewer.features.gumball';
 
 (<any>window).SDV = SDV;
 (<any>window).SDVInteractions = SDVInteractions;
 (<any>window).SDVAttributeVisualization = SDVAttributeVisualization;
 (<any>window).SDVDrawingTools = SDVDrawingTools;
+(<any>window).SDVGumball = SDVGumball;
+
