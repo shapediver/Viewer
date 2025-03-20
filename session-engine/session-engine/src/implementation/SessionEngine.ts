750acb0d62c882ebc268704544b77134090c26e2
Michael Oppitz <michael@shapediver.com>
2025-03-20 14:31:52 +0100

    debug

--- session-engine/session-engine/src/implementation/SessionEngine.ts
+++ session-engine/session-engine/src/implementation/SessionEngine.ts
@@ -670,6 +670,7 @@ export class SessionEngine implements ISessionEngine {
 
             // call the update callbacks
             if (waitForViewportUpdate === false) {
+                console.log('updating outputs via events');
                 for (const outputId in this.outputs) {
                     if (oldOutputVersions[outputId] !== newOutputVersions[outputId]) {
                         this._eventEngine.emitEvent(EVENTTYPE.OUTPUT.OUTPUT_UPDATED, <IOutputEvent>{
@@ -680,6 +681,8 @@ export class SessionEngine implements ISessionEngine {
                         });
                     }
                 }
+                console.log('finished updating outputs via events');
+
 
                 await this.waitForUpdateCallbacks(newOutputVersions, oldOutputVersions, newNode, oldNode);
 
@@ -2083,8 +2086,12 @@ export class SessionEngine implements ISessionEngine {
     }
 
     private async waitForUpdateCallbacks(newOutputVersions: { [key: string]: string }, oldOutputVersions: { [key: string]: string }, newNode: ITreeNode, oldNode: ITreeNode) {
+        console.log('updating session via update callback');
+
         // call the update callback function on the session
         if (this._updateCallback) await Promise.resolve(this._updateCallback(newNode, oldNode));
+        console.log('finished updating session via update callback');
+        console.log('updating outputs via update callback');
 
         const promises = [];
         // call the update callback functions on the outputs
@@ -2099,6 +2106,8 @@ export class SessionEngine implements ISessionEngine {
             }
         }
         await Promise.all(promises);
+        console.log('finished updating outputs via update callback');
+
     }
 
     // #endregion Private Methods (18)
