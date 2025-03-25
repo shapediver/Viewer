795decd0df7ecf68356c8eb2b80b2a58acd2c6ec
Michael Oppitz <michael@shapediver.com>
2025-03-25 11:19:40 +0100

    prettier setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,35 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  root: true,
-  overrides: [
-    {
-      files: ['*.{ts,tsx}'],
-      rules: {
-        '@typescript-eslint/switch-exhaustiveness-check': 'error',
-      },
-      parser: '@typescript-eslint/parser',
-      parserOptions: {
-        'project': './tsconfig.json'
-      }
-    }
-  ],
-  rules: {
-    '@typescript-eslint/no-inferrable-types': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/no-empty-function': 'off',
-    'quotes': ['warn', 'single'],
-    'semi': ['warn', 'always']
-  }
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:prettier/recommended",
+		"prettier",
+	],
+	parser: "@typescript-eslint/parser",
+	plugins: ["@typescript-eslint", "eslint-plugin-prettier"],
+	root: true,
+	overrides: [
+		{
+			files: ["*.{ts,tsx}"],
+			rules: {
+				"@typescript-eslint/switch-exhaustiveness-check": "error",
+			},
+			parser: "@typescript-eslint/parser",
+			parserOptions: {
+				project: "./tsconfig.json",
+			},
+		},
+	],
+	rules: {
+		"prettier/prettier": "error",
+		"@typescript-eslint/no-inferrable-types": "off",
+		"@typescript-eslint/no-non-null-assertion": "off",
+		"@typescript-eslint/no-empty-function": "off",
+		"linebreak-style": ["error", "windows"],
+		quotes: ["error", "double"],
+		semi: ["error", "always"],
+		"@typescript-eslint/no-explicit-any": 0,
+		"no-debugger": 0,
+	},
 };
