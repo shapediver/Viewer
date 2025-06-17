9fc4f299a99999d547b46c11248ba52957a23c2f
Michael Oppitz <michael@shapediver.com>
2025-06-17 13:31:41 +0200

    automatic pre-publishing commit

--- shared/build-data/src/build_data.ts
+++ shared/build-data/src/build_data.ts
@@ -1,1 +1,1 @@
-export const build_data = {"build_version":"3.3.10.1-rc.3","build_date":"2025-05-28T14:57:50.932Z","build_branch":"development","build_commit":"fa6e8ff47d46ed855fe4ad812866ed9f5e2670e6"};
+export const build_data = {"build_version":"3.3.11.0-rc.0","build_date":"2025-06-17T11:29:22.774Z","build_branch":"development","build_commit":"4ce5389cf1046ca97c8c25e9a3e7155f591b09d6"};
