d237e4ebcc86414242448d64fc8e87c0d6000f58
Michael Oppitz <michael@shapediver.com>
2026-02-13 09:29:38 +0100

    material DB cleanup

--- session-engine/session-engine/src/implementation/OutputLoader.ts
+++ session-engine/session-engine/src/implementation/OutputLoader.ts
@@ -319,9 +319,6 @@ export class OutputLoader {
 				await this._globalAccessObjects.assignMaterialFromDatabase(
 					Tree.instance.root,
 				);
-				await this._globalAccessObjects.assignMaterialFromDatabase(
-					node,
-				);
 			} else {
 				// otherwise, only to the newly loaded output node
 				await this._globalAccessObjects.assignMaterialFromDatabase(
